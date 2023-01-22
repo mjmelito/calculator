f9bd3ca9661a837777cc871c990b93b903b9f63b
Matt Melito <matt_melito@outlook>
2023-01-21 22:11:40 -0800

    Add +, -, *, and / functions, and combines response into one alert.

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -1,9 +1,21 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
+// business logic
+function add(num1, num2) {
+  return num1 + num2;
+}
+function sub(num1, num2) {
+  return num1 - num2;
+}
+function mult(num1, num2) {
+  return num1 * num2;
+}
+function div(num1, num2) {
+  return num1 / num2;
 }
 
-function add(number1, number2) {
-  return number1 + number2;
-}
-
-saySomething("The sum is " + add(3, 5) + ".");
+// user interface logic 
+const num1 = parseInt(prompt("Enter a number:"));
+const num2 = parseInt(prompt("Enter another number:"));
+window.alert(num1 + " + " + num2 + " = " + add(num1, num2) + ". " +
+num1 + " - " + num2 + " = " + sub(num1, num2) + ". " +
+num1 + " * " + num2 + " = " + mult(num1, num2) + ". " +
+num1 + " / " + num2 + " = " + div(num1, num2) + ". ");
